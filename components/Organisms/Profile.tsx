f382a3b86e283ac259456b7bfc5f6dac1e9210be
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2023-05-30 22:10:36 -0400

    edit the style of the components

--- components/Organisms/Profile.tsx
+++ components/Organisms/Profile.tsx
@@ -1,34 +1,91 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Button, Avatar, TextInput } from 'react-native-paper'
-import FullName from '../Atoms/FullName'
-import LastName from '../Atoms/LastName'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button, Avatar, TextInput, Switch } from 'react-native-paper';
+import FullName from '../Atoms/FullName';
+import LastName from '../Atoms/LastName';
 
 export default function Profile() {
   return (
-	<View>
-		<Avatar.Image source={null} />
-	<Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-	Change Picture
-  	</Button>
-	  <TextInput
-      label="First Name"
-      value={null}
-      onChangeText={null}
-	  />
-	   <TextInput
-      label="Last Name"
-      value={null}
-      onChangeText={null}
-	  />
-	<Text> Notification</Text>
-	
-
-
-
-	
-	</View>
-  )
+    <View style={styles.container}>
+      <View style={[styles.imageContainer, { flex: 1 }]}>
+        <Avatar.Image source={null} />
+        <Button
+          style={{ width: 200, height: 50 }}
+          icon='camera'
+          mode='contained'
+          onPress={() => console.log('Pressed')}
+        >
+          Change Picture
+        </Button>
+      </View>
+      <View style={{ flex: 1, columnGap: 10 }}>
+        <TextInput label='First Name' value={null} onChangeText={null} />
+        <TextInput label='Last Name' value={null} onChangeText={null} />
+      </View>
+      <View style={styles.notificationContainer}>
+        <Text> Notifications </Text>
+        <View style={styles.switchContainer}>
+          <Text style={styles.switchText}> Order Statues
+            <Switch
+              style={styles.switch}
+              value={false}
+              onValueChange={null}
+            ></Switch>
+          </Text>
+        </View>
+        <View style={styles.switchContainer}>
+          <Text style={styles.switchText}> Special Offers
+            <Switch
+              style={styles.switch}
+              value={false}
+              onValueChange={null}
+            ></Switch>
+          </Text>
+        </View>
+        <View style={styles.switchContainer}>
+          <Text style={styles.switchText}> News Letter 
+            <Switch
+              style={styles.switch}
+              value={false}
+              onValueChange={null}
+            ></Switch>
+          </Text>
+        </View>
+      </View>
+      <Button  mode='contained' onPress={() => console.log('Pressed')}>
+        Log Out
+      </Button>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+
+  imageContainer: {
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  notificationContainer: {
+    flex: 3,
+	justifyContent: 'center',
+	alignItems: 'center'
+  },
+  switchContainer: { 
+
+
+	
+  },
+  switchText: {
+paddingRight: 50,
+
+	
+  },
+  switch: {
+	
+    
+  },
+});
