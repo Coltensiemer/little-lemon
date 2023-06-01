27f1bc978f0cecd29c0ab60b442ec0738b5c05e1
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2023-05-31 23:22:42 -0400

    added more styling to container

--- components/Organisms/Profile.tsx
+++ components/Organisms/Profile.tsx
@@ -1,16 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, } from 'react-native';
 import React from 'react';
-import { Button, Avatar, TextInput, Switch } from 'react-native-paper';
+import { Button, Avatar, TextInput, Switch, Divider } from 'react-native-paper';
 import FullName from '../Atoms/FullName';
 import LastName from '../Atoms/LastName';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Profile() {
   return (
-    <View style={styles.container}>
-      <View style={[styles.imageContainer, { flex: 1 }]}>
-        <Avatar.Image source={null} />
+    <ScrollView style={styles.container}
+    contentContainerStyle={styles.containContainer}>
+      <View style={[styles.imageContainer, {  }]}>
+        <Avatar.Image style={{alignSelf:'center', justifyContent: 'center', width: 128, height: 128}} source={null} />
         <Button
-          style={{ width: 200, height: 50 }}
+          style={{ width: 200, height: 50, justifyContent:'center', alignSelf:'center', marginTop: 20}}
           icon='camera'
           mode='contained'
           onPress={() => console.log('Pressed')}
@@ -18,14 +20,17 @@ export default function Profile() {
           Change Picture
         </Button>
       </View>
-      <View style={{ flex: 1, columnGap: 10 }}>
+      <View >
         <TextInput label='First Name' value={null} onChangeText={null} />
         <TextInput label='Last Name' value={null} onChangeText={null} />
+        <TextInput label='Email' value={null} onChangeText={null} />
       </View>
       <View style={styles.notificationContainer}>
         <Text> Notifications </Text>
         <View style={styles.switchContainer}>
-          <Text style={styles.switchText}> Order Statues
+          <Text style={styles.switchText}>
+            {' '}
+            Order Statues
             <Switch
               style={styles.switch}
               value={false}
@@ -34,7 +39,9 @@ export default function Profile() {
           </Text>
         </View>
         <View style={styles.switchContainer}>
-          <Text style={styles.switchText}> Special Offers
+          <Text style={styles.switchText}>
+            {' '}
+            Special Offers
             <Switch
               style={styles.switch}
               value={false}
@@ -43,7 +50,9 @@ export default function Profile() {
           </Text>
         </View>
         <View style={styles.switchContainer}>
-          <Text style={styles.switchText}> News Letter 
+          <Text style={styles.switchText}>
+            {' '}
+            News Letter
             <Switch
               style={styles.switch}
               value={false}
@@ -52,40 +61,57 @@ export default function Profile() {
           </Text>
         </View>
       </View>
-      <Button  mode='contained' onPress={() => console.log('Pressed')}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+      <Button  mode="elevated" onPress={() => console.log('Pressed')}>
+    Discard Changes
+    </Button>
+    <Button  mode='contained-tonal' onPress={() => console.log('Pressed')}>
+    Save Changes
+    </Button>
+        </View>
+        
+        <Divider /> 
+      <Button
+        style={{ margin: 50, width: 200, height: 50, alignSelf: 'center', justifyContent: 'center' }}
+        mode='contained'
+        onPress={() => console.log('Pressed')}
+      >
         Log Out
       </Button>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-around',
+    height: '20%',
+    width: '80%',
+  alignSelf: 'center'    
+  },
+  containContainer: { 
+justifyContent: 'center',
+
   },
 
   imageContainer: {
-    flexDirection: 'row',
-    marginTop: 10,
+    flexDirection: 'column',
+    alignItems: null,
+    margin: 20,
   },
   notificationContainer: {
-    flex: 3,
-	justifyContent: 'center',
-	alignItems: 'center'
+   
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    margin: 48,
   },
-  switchContainer: { 
+  switchContainer: {
 
-
-	
-  },
-  switchText: {
-paddingRight: 50,
-
-	
-  },
-  switch: {
-	
+   // Adjust the space for Notifications 
     
   },
+  switchText: {
+    paddingRight: 50,
+  },
+  switch: {},
 });
