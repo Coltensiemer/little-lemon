b0fe1d7d9f7986d859779b9c0eda1c10dabd9aa9
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2023-08-01 23:30:38 -0500

    worked on sign up modal and style

--- components/Organisms/UserSignUp.tsx
+++ components/Organisms/UserSignUp.tsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-import { TextInput, Button, Title, Chip, Switch } from 'react-native-paper';
+import { TextInput, Button, Title, Chip, Switch, Portal, Modal } from 'react-native-paper';
 import { useForm, Controller, useFormState } from 'react-hook-form';
 
 interface userData {
-  firstName: string
-  lastName: string
-  Email: string
-  Password1: string 
+  firstName: string;
+  lastName: string;
+  Email: string;
+  Password1: string;
 }
 
-export default function UserSignUp() {
-  const [passwordViewer, setPasswordViewer] = useState<boolean>(true)
+export default function UserSignUp({navigation}) {
+  const [passwordViewer, setPasswordViewer] = useState<boolean>(true);
+  const [visibleModal, setVisibleModal] = useState<boolean>(true); 
+
   const {
     control,
     handleSubmit,
@@ -22,38 +24,34 @@ export default function UserSignUp() {
 
   const password1 = watch('Password1');
 
-
   const handleSignIn = async (data: userData) => {
-  console.log(data)
-    try { 
-      const formatData = { 
+    console.log(data);
+    try {
+      const formatData = {
         first_name: data.firstName,
         last_name: data.lastName,
         email: data.Email,
-        password: data.Password1
-      }
+        password: data.Password1,
+      };
 
-      const options = { 
+      const options = {
         method: 'POST',
-        headers: {'content-Type': 'application/json'},
-        body: JSON.stringify(formatData)
-      }
+        headers: { 'content-Type': 'application/json' },
+        body: JSON.stringify(formatData),
+      };
 
-      const response = await fetch( 
-        'http://localhost:3100/users', options
-      )
+      const response = await fetch('http://localhost:3100/users', options);
 
-      const responseData = await response.json(); 
-      console.log('POST request successful')
-    }
-    catch (error) {
-     console.log('Error:', error)
+      const responseData = await response.json();
+      console.log('POST request successful');
+    } catch (error) {
+      console.log('Error:', error);
     }
   };
 
   return (
-    <View style={null}>
-      <Title style={null}>User Sign Up</Title>
+    <View style={{ flex: 1, margin: 20 }}>
+      <Title style={{ fontWeight: 'bold' }}>User Sign Up</Title>
       {/* FIRST NAME INPUT */}
       <Controller
         name='firstName'
@@ -96,91 +94,111 @@ export default function UserSignUp() {
           />
         )}
       />
-        <Controller
-            control={control}
-            name='Email'
-            defaultValue=''
-            rules={{
-              required: {
-                value: true,
-                message: 'Email is required',
-              },
-              pattern: {
-                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                message: 'Email is invalid',
-              },
-            }}
-            render={({ field: { onChange, onBlur, value } }) => (
-      <TextInput
-        label='Email'
-        error={!!errors.Email}
-        onChangeText={(text) => onChange(text)}
-        style={null}
-        mode='outlined'
-      />
-      )}
+      <Controller
+        control={control}
+        name='Email'
+        defaultValue=''
+        rules={{
+          required: {
+            value: true,
+            message: 'Email is required',
+          },
+          pattern: {
+            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            message: 'Email is invalid',
+          },
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label='Email'
+            error={!!errors.Email}
+            onChangeText={(text) => onChange(text)}
+            style={null}
+            mode='outlined'
+          />
+        )}
       />
 
-{/* PASSWORD INPUT  */}
-<View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
-  <Text> Minimum eight characters, at least one letter and one number:</Text>
-  <Switch
-  value={!passwordViewer}
-onValueChange={() => setPasswordViewer(!passwordViewer)}
-  ></Switch>
-</View>
-<Controller
-            control={control}
-            name='Password1'
-            defaultValue=''
-            rules={{
-              required: {
-                value: true,
-                message: 'Password is required',
-              },
-              pattern: {
-                value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-                message: 'Password is invalid',
-              },
-            }}
-            render={({ field: { onChange, onBlur, value } }) => (
-      <TextInput
-        label='Password'
-        error={!!errors.Password1}
-        onChangeText={(text) => onChange(text)}
-        secureTextEntry={passwordViewer}
-        style={null}
-        mode='outlined'
+      {/* PASSWORD INPUT  */}
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          marginTop: 20,
+        }}
+      >
+        <Text style={{ fontSize: 10 }}>
+          {' '}
+          Minimum eight characters, at least one letter and one number:
+        </Text>
+        <Switch
+          value={!passwordViewer}
+          onValueChange={() => setPasswordViewer(!passwordViewer)}
+        ></Switch>
+      </View>
+      <Controller
+        control={control}
+        name='Password1'
+        defaultValue=''
+        rules={{
+          required: {
+            value: true,
+            message: 'Password is required',
+          },
+          pattern: {
+            value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+            message: 'Password is invalid',
+          },
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label='Password'
+            error={!!errors.Password1}
+            onChangeText={(text) => onChange(text)}
+            secureTextEntry={passwordViewer}
+            style={null}
+            mode='outlined'
+          />
+        )}
       />
-      )}
-      />
-     
-<Controller
-            control={control}
-            name='Password2'
-            defaultValue=''
-            rules={{
-              required: {
-                value: true,
-                message: 'Password is required',
-              },
-             validate: (value) => value === password1
-            }}
-            render={({ field: { onChange, onBlur, value } }) => (
-      <TextInput
-        label='Confirm Password'
-        error={!!errors.Password2}
-        onChangeText={(text) => onChange(text)}
-        secureTextEntry={passwordViewer}
-        mode='outlined'
-      />
-      )}
-      />
-   
 
- 
-      
-      <Button mode='contained' onPress={handleSubmit(handleSignIn)} style={null}>
+      <Controller
+        control={control}
+        name='Password2'
+        defaultValue=''
+        rules={{
+          required: {
+            value: true,
+            message: 'Password is required',
+          },
+          validate: (value) => value === password1,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label='Confirm Password'
+            error={!!errors.Password2}
+            onChangeText={(text) => onChange(text)}
+            secureTextEntry={passwordViewer}
+            mode='outlined'
+          />
+        )}
+      />
+      <Portal>
+        <Modal visible={visibleModal}
+        onDismiss={() => setVisibleModal(false)}
+        contentContainerStyle={{backgroundColor: 'white',
+        padding: 20,
+        margin: 10,}}>
+          <Text style={{fontSize: 16, fontWeight: 'bold', justifyContent: 'center', alignSelf:  'center', margin: 40 }}>Congrats on Signing Up!</Text>
+
+          <Button mode='contained' style={{margin: 10}} onPress={() => {navigation.navigate('Sign In')}}> Continue</Button>
+        </Modal>
+      </Portal>
+      <Button
+        mode='contained'
+        onPress={handleSubmit(handleSignIn)}
+        style={{ marginTop: 20 }}
+      >
         Sign Up
       </Button>
     </View>
